9f2ebbb3050f435550816efb9d3d717fb77be94e
Raquel Azcue <rvazcue@gmail.com>
2018-07-28 11:18:55 -0500

    sending basic files

---
+++ server.js
@@ -0,0 +1,7 @@
+//Here we start by downloading the 
+//npm modules listed below
+var fs = require("fs");
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var path = require("path");
