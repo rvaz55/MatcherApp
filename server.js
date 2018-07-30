32df97080cd963a657243f5ac87d577c6ebbf040
Raquel Azcue <rvazcue@gmail.com>
2018-07-30 09:52:27 -0500

    setting up the routes

--- server.js
+++ server.js
@@ -2,6 +2,28 @@
 //npm modules listed below
 var fs = require("fs");
 var express = require('express')
-var app = express()
 var bodyParser = require('body-parser')
 var path = require("path");
+
+//Creating the instance of express and 
+//storing it in the app variable then naming the port
+var app = express()
+var PORT = 3000
+
+//Then we make sure that the server we created 
+//is LISTENING 
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+});
+
+//Here we create two routes: 'survey' & 'home'
+app.get('/home', function(req,res){
+    console.log("this is the home route")
+});
+app.get('/survey', function(req,res){
+    console.log("this is the survey route")
+
+});
+    
+    
+   
